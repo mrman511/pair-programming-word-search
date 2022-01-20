21468643b2248748823de00bd240dd3e95439c55
Paul Bodner <bodner80@gmail.com>
2022-01-19 18:06:02 -0800

    paired programming crossword

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    // transpose arrays to vertical letters
+    let verticleArray = transpose(letters)
+
+    const verticleJoin = verticleArray.map(ls => ls.join(''))
+    //console.log(verticleJoin);
+    for (l of verticleJoin) {
+        if (l.includes(word)) return true
+    }
+
+    return false;
+
+    //return grid
+}
+
+const transpose = (array) => {
+
+    let newGrid = []
+    for (let row in array) {
+        //for each row create new array
+        let newOrder = [];
+        for (let i = 0; i < array.length; i++) {
+            newOrder.push(array[i][row])
+        }
+        newGrid.push(newOrder);
+    }
+    return newGrid;
 }
 
 module.exports = wordSearch
+
+wordSearch([[1, 2, 3], [1, 2, 3], [1, 2, 3,]])
